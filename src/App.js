200f6e77cc322824f8d5a48f31737a765d9c5b6e
Marie-Elize Venter <ferplie@gmail.com>
2023-01-16 17:28:33 +0200

    Zoho Widget Basic Opening

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState, useEffect, useRef } from "react";
+import Spinner from "./Components/Spinner";
 
 function App() {
+  useEffect(() => {});
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Hallo</h1>
+        <div>
+          <Spinner />
+        </div>
       </header>
     </div>
   );
---
+++ src/Components/Spinner.js
@@ -0,0 +1,7 @@
+import React from "react";
+import "./Spinner.css";
+function Spinner() {
+  return <div className="Spinner">This is the spinner</div>;
+}
+
+export default Spinner;
