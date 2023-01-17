69e8d1521606e8d2aa6a8689c7214ad00e8973f3
Marie-Elize Venter <ferplie@gmail.com>
2023-01-17 07:02:19 +0200

    BASIC SETUP

--- src/App.js
+++ src/App.js
@@ -1,19 +1,35 @@
+/* eslint-disable */
+
 import logo from "./logo.svg";
 import "./App.css";
 import { useState, useEffect, useRef } from "react";
 import Spinner from "./Components/Spinner";
 
 function App() {
-  // useEffect(() => {
-  //   ZOHO.CREATOR.init().then(function (data) {
-  //     //Code goes here
-  //   });
-  // });
+  const [name, setName] = useState("UNKNOWN USER");
+  useEffect(() => {
+    ZOHO.CREATOR.init().then(function (data) {
+      var config = {
+        appName: "bainsvleicpf",
+        reportName: "All_HouseHold_People",
+        criteria: "(ID == 2573309000007572638)",
+        page: 1,
+        pageSize: 10,
+      };
+
+      ZOHO.CREATOR.API.getAllRecords(config).then(function (response) {
+        var recordArr = response.data;
+        console.log(recordArr[0]);
+        setName(recordArr[0].Name);
+        // myName.innerHTML = recordArr[0].Full_Name;
+      });
+    });
+  }, []);
 
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hallo</h1>
+        <h1>Hallo {name}</h1>
         <div>
           <Spinner />
         </div>
