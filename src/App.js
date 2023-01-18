7b4f78905ebb927e342b71361ac346ef7100d41e
Marie-Elize Venter <ferplie@gmail.com>
2023-01-18 09:14:37 +0200

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,30 +1,11 @@
 /* eslint-disable */
 
-import logo from "./logo.svg";
 import "./App.css";
 import { useState, useEffect, useRef } from "react";
 import Spinner from "./Components/Spinner";
 
 function App() {
   const [name, setName] = useState("UNKNOWN USER");
-  useEffect(() => {
-    ZOHO.CREATOR.init().then(function (data) {
-      var config = {
-        appName: "bainsvleicpf",
-        reportName: "All_HouseHold_People",
-        criteria: "(ID == 2573309000007572638)",
-        page: 1,
-        pageSize: 10,
-      };
-
-      ZOHO.CREATOR.API.getAllRecords(config).then(function (response) {
-        var recordArr = response.data;
-        console.log(recordArr[0]);
-        setName(recordArr[0].Name);
-        // myName.innerHTML = recordArr[0].Full_Name;
-      });
-    });
-  }, []);
 
   return (
     <div className="App">
--- src/Components/Spinner.js
+++ src/Components/Spinner.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./Spinner.css";
 function Spinner() {
-  return <div className="Spinner">This is the spinner</div>;
+  return <div className="Spinner">THIS IS ANOTHER COMPONENT</div>;
 }
 
 export default Spinner;
